79fdcae5d09bc481a0d3a55542f8641c30b2533b
HCR-Rgb12 <andy0664@mju.ac.kr>
2026-04-06 16:47:01 +0900

    1차과제 원본 파일입니다

--- main.c
+++ main.c
@@ -1,22 +1,51 @@
-//1.주석 
-/*
-	프로그램명:main.c
-	작 성 자 :류형찬 26-03-23
-	하 는 일 :나의 디지털 명함 만들기-표준출력함수 연습
+/* 
+  파일이름: main.c
+  작 성 자:60232361 류형찬
+  하 는 일: RPG 형식의 캐릭터 생성 및 보스 잡기 게임 1.0
 */
-
-
 #include <stdio.h>
 
-int main(void) 
-{
-	system("chcp 65001");
-	printf("================\n");
-	printf("[나의 디지털 명함]\n");
-	printf("이름\t:류형찬\n");
-	printf("전공\t:정보통신공학\n");
-	printf("소개\t:열심히 배우겠습니다.\n");
-	
-	return 0;
+int main(){
+  char name;
+  int job;// 직업(추후 번호가 아니라 직업이름으로 표시할 예정입니다)
+  int hp; //체력
+  double ad; //물리 공격력
+  double ap; //마법 공격력
+  //int faith; 신앙심(성직자의 회복스텟)(추후 추가예정입니다)
+  double power;// 종합 전투력
+  //첫번째 보스 체력 및 공격력
+  int b1_hp;
+  int b1_ad; 
+  b1_hp=120;
+  b1_ad=15;
+  printf("보스 잡기 게임에 오신 걸 환영합니다!\n");
+  printf("===============================\n");
+  printf("캐릭터의 이름을 입력해주세요:");
+  scanf(" %c", &name);
+  printf("원하는 직업을 선택해주세요:1.전사,2.마법사,3.성직자:");
+  scanf("%d", &job);
+  printf("스텟 입력창입니다\n");
+  printf("체력:");
+  scanf("%d", &hp);
+  printf("공격력:");
+  scanf("%lf", &ad);
+  printf("마력:");
+  scanf("%lf", &ap);
+  
+  power = hp + ad + ap; //총 전투력 계산식
 
+  printf("현재 캐릭터의 스텟 상황입니다\n");
+  printf("이름:%c\n",name);
+  printf("직업(1.전사,2.마법사,3.성직자):%d\n",job);
+  printf("공격력:%.2f\n",ad);
+  printf("마력:%.2f\n",ap);
+  printf("총 전투력:%.2lf\n",power);
+  
+  printf("==============================\n");
+  printf("첫번째 보스등장!\n");
+  printf("보스의 체력:%d\n",b1_hp);
+  printf("보스의 공격력:%d\n",b1_ad);
+  printf("전투는 차후 구현됩니다. 업데이트를 기다려주세요");
+  
+  return 0;
 }
