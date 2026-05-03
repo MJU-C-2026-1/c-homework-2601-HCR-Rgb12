2fabed999278f6d9caefa78cd2c739af6c284892
HCR-Rgb12 <andy0664@mju.ac.kr>
2026-05-03 02:24:02 +0000

    2차과제

--- main.c
+++ main.c
@@ -14,18 +14,19 @@ int main(){
   double faith; //신앙심(성직자의 주요스텟)
   double power;// 종합 전투력
   int hunt_try;//사냥터 진행 횟수
-  int i;
-  //첫번째 보스 체력 및 공격력
+  int i; //반복문 변수
+  //첫번째 보스의 체력 및 전투력
+  int b1_power;
   int b1_hp;
-  int b1_ad; 
-  b1_hp=120;
-  b1_ad=15;
+  b1_hp=800;
+  b1_power=1300;
+
   int try; //보스 입장 여부 확인 변수
   int level=1;//직업 레벨
   int exp=0;//경험치량
 
 
-  printf("보스 잡기 게임에 오신 걸 환영합니다!\n");
+  printf("보스 토벌 게임에 오신 걸 환영합니다!\n");
   printf("===============================\n");
   printf("캐릭터의 이름을 입력해주세요:");
   scanf(" %c", &name);
@@ -34,11 +35,11 @@ int main(){
   switch(job){
     case 1:
       printf("전사를 고르셨습니다\n");
-      printf("체력 입력(1포인트당15):");  
+      printf("체력 입력(1포인트당1.5):");  
       scanf("%d",&hp);
-      printf("공격력 입력(1포인트당 1.2):");
+      printf("공격력 입력(1포인트당 1.3):");
       scanf("%lf" , &ad);
-      power= ad*1.3 + hp*15;
+      power= ad*1.3 + hp*1.5;
       break;
 
     case 2:
@@ -52,18 +53,18 @@ int main(){
 
     case 3:
       printf("성직자를 고르셨습니다\n");
-      printf("체력 입력(1포인트당 1):");  
+      printf("체력 입력(1포인트당 1.2):");  
       scanf("%d",&hp);
-      printf("공격력 입력(1포인트당 1.4):");
+      printf("공격력 입력(1포인트당 1.2):");
       scanf("%lf",&ad);
-      printf("마력 입력(1포인트당 1.5):");
+      printf("마력 입력(1포인트당 1.4):");
       scanf("%lf",&ap);
       faith=1.4*ad + 1.5*ap;
       power = faith + hp;
       break;
 
   }
-
+  printf("");
   printf("현재 캐릭터의 스텟 상황입니다\n");
   printf("이름:%c\n",name);
   printf("직업(1.전사,2.마법사,3.성직자):%d\n",job);
@@ -79,13 +80,13 @@ int main(){
   scanf("%d",&boss_start);
   if(boss_start==1)
   {
-    printf("사냥터에 오신 것을 환영합니다!");
-    printf("사냥횟수를 입력하세요\n");
+    printf("사냥터에 오신 것을 환영합니다!");//사냥터를 통한 경험치 획득 및 레벨업 수식
+    printf("사냥횟수를 입력하세요:");
     scanf("%d", &hunt_try);
     for(i=1;i<=hunt_try;i++){
       int gain=25;//획득 경험치
       exp+=gain;
-      printf("%d만큼의 경험치를 힉득하였습니다\n",gain);
+      printf("%d만큼의 경험치를 획득하였습니다\n",gain);
       if(exp>=100){
         printf("레벨업 하였습니다\n");
         exp-=100;
@@ -94,7 +95,7 @@ int main(){
           case 1:
             hp+=20;
             ad+=7;
-            power= ad*1.3 + hp*15;
+            power= ad*1.3 + hp*1.5;
             break;
           case 2:
             hp+=10;
@@ -112,31 +113,37 @@ int main(){
     }
   }
   
-  else if(boss_start==2)
+  else if(boss_start==2)//보스 레이드 시작 부분
   {
     printf("보스레이드를 시작합니다!\n");
   }
   else
   {
     printf("메뉴화면으로 돌아갑니다\n");
+    return 0;
   }
   
   printf("==============================\n");
   printf("첫번째 보스등장!\n");
   printf("보스의 체력:%d\n",b1_hp);
-  printf("보스의 공격력:%d\n",b1_ad);
-  printf("도전하시겠습니까?");
+  printf("보스의 전투력:%d\n",b1_power);
+  printf("도전하시겠습니까?(예:1 ,아니오:2)");
   scanf("%d",&try);
   if(try== 1)
   {
     printf("전투를 시작합니다\n");
-    if(power >= b1_hp){
+    if(power >= b1_power){
       printf("승리! 다음보스로 넘어갑니다\n");
     }
     else{
       printf("패배...다시 도전해보세요\n");
     } 
   }
+  else if(try == 2){
+    printf("게임을 중지하고 메뉴로 돌아갑니다\n");
+    
+  } 
+  
   return 0;
   
 
